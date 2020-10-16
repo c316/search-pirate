4a3f515d8446035ad90660cbece4451d8ecf80d7
Joshua Bechard <josh.bechard@jewsforjesus.org>
2020-10-15 22:42:01 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  let searchValue = '';
+	function search() {
+    console.log('searching');
+    console.log(searchValue)
+  }
+</script>
+
+<main>
+	<h1>Search Pirate!</h1>
+	<label for="search">Search</label>
+	<input id="search" bind:value={searchValue} />
+	<button on:click={search}>Search</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif, cursive;
+	}
+
+	h1 {
+		color: #760a0a;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
